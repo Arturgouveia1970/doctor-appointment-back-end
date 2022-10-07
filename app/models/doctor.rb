1d56365865da90e374c2873211edf67c61baf64d
principles31 <principles31@gmail.com>
2022-10-07 03:09:41 -0700

    Add doctor class model and validations

---
+++ app/models/doctor.rb
@@ -0,0 +1,13 @@
+class Doctor < ApplicationRecord
+   has_many :reservations, dependent: :destroy
+   has_many :users, through: :reservations
+   belongs_to :category
+   validates :name, presence: true
+   validates :price, presence: true
+   validates :description, presence: true
+   validates :country, presence: true
+   validates :city, presence: true
+   validates :address, presence: true
+   validates :image_url, presence: true
+end
+ 
