6746334b1aa0670ba548402633db5d329797a24c
principles31 <principles31@gmail.com>
2022-10-08 13:07:34 -0700

    Fix rocbocup error

--- app/controllers/api/v1/doctors-controller.rb
+++ app/controllers/api/v1/doctors-controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::DoctorsController < ApplicationController
-    def index
+  def index
       @doctors = Doctor.all
       render json: { status: 'Success', message: 'loaded doctors', doctors: @doctors }, status: :ok
     end
-  
-    def show
+
+  def show
       @doctor = Doctor.includes(:reservations).find(params[:id])
       @reservations = @doctor.reservations.order(created_at: :desc)
       render json: { status: 'Success', message: 'loaded doctor', doctor: @doctor, reservations: @reservations },
              status: :ok
     end
-  
-    def create
+
+  def create
       @doctor = Doctor.new(doctor_params)
       if @doctor.save
         render json: {
@@ -23,8 +23,8 @@ class Api::V1::DoctorsController < ApplicationController
         }, status: :unprocessable_entity
       end
     end
-  
-    def destroy
+
+  def destroy
       @doctor = Doctor.find(params[:id])
       if @doctor.destroy
         render json: { message: 'Doctor deleted succesfully.' }
@@ -32,8 +32,8 @@ class Api::V1::DoctorsController < ApplicationController
         render json: { message: 'Something went wrong' }, status: :unprocessable_entity
       end
     end
-  
-    def update
+
+  def update
       @doctor = Doctor.find(params[:id])
       if @doctor.update(doctor_params)
         render json: { message: 'Doctor updated succesfully.' }
@@ -41,11 +41,10 @@ class Api::V1::DoctorsController < ApplicationController
         render json: { message: 'Something went wrong' }, status: :unprocessable_entity
       end
     end
-  
+
     private
   
-    def doctor_params
+  def doctor_params
       params.require(:doctor).permit(:name, :price, :description, :country, :city, :address, :image_url)
     end
-  end
-  
+end
