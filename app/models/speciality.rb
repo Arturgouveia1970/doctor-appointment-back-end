8910be2130da6eca61c383a2cfbd2c064a877697
principles31 <principles31@gmail.com>
2022-10-10 05:59:57 -0700

    Set validations and associations

--- app/models/speciality.rb
+++ app/models/speciality.rb
@@ -1,7 +1,7 @@
-class Category < ApplicationRecord
+class speciality < ApplicationRecord
     validates :name, :description, presence: true
     validates :rating, length: { maximum: 1 }, numericality: { less_than_or_equal_to: 5 }, allow_nil: true
   
-    has_many :hotels, dependent: :destroy
+    has_many :doctors, dependent: :destroy
   end
   
